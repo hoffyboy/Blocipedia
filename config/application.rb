2db32d0e3391e96c56230cd971ab4fa5a9c921fc
Zalmi Hofstatter <hoffyboy@gmail.com>
2018-03-12 18:51:48 -0700

    Added sendgrid

--- config/application.rb
+++ config/application.rb
@@ -21,14 +21,17 @@ module WikiIt
       g.controller_specs false
     end
 
+
+  if Rails.env.development? || Rails.env.production?
+    ActionMailer::Base.delivery_method = :smtp
     ActionMailer::Base.smtp_settings = {
-        address:        'smtp.gmail.com',
-        port:           '587',
-        authentication: :plain,
-        user_name:      ENV['SENDGRID_USERNAME'],
-        password:       ENV['SENDGRID_PASSWORD'],
-        domain:         'mail.google.com',
-        enable_starttls_auto: true
-      }
+      address:        'smtp.sendgrid.net',
+      port:           '2525',
+      authentication: :plain,
+      user_name:      ENV['SENDGRID_USERNAME'],
+      password:       ENV['SENDGRID_PASSWORD'],
+      domain:         'heroku.com',
+      enable_starttls_auto: true
+    }
   end
 end
